b114225a1fe66afcd2bec7c71bf3c06c8de01f85
szegedinorbert <43540117+szegedinorbert@users.noreply.github.com>
2018-10-12 23:00:25 +0200

    Add files via upload

---
+++ hf2.1/GeometricShape.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+
+    import java.util.Date;
+
+public class GeometricShape {
+ 
+    private String color="white";
+    private boolean filled=false;
+    private Date dateCreated=new Date();
+
+    public GeometricShape() {
+    }
+
+    public GeometricShape(String color, boolean filled) 
+    {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() 
+    {
+        return color;
+    }
+
+    public void setColor(String color) 
+    {
+        this.color = color;
+    }
+
+    public boolean isFilled() 
+    {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) 
+    {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() 
+    {
+        return dateCreated;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "színe=" + color + ", kitöltése=" + filled + ", dátuma=" + dateCreated;
+    }
+}
+
